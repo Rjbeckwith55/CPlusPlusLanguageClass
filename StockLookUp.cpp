d3c74fb736db772a3ba0435855a24527cbce0c42
Robert Beckwith <rjbeckwith55@gmail.com>
2017-03-22 00:49:07 -0500

    Formatted all the output and added a linear search

--- StockLookUp.cpp
+++ StockLookUp.cpp
@@ -11,6 +11,7 @@ Descr: Program that reads the stock data into parallel arrays and outputs it
 #include <string>
 #include <iomanip>
 #include <memory>
+#include <locale>
 using namespace std;
 
 /*Main function to open file, create and manage dynamic arrays, read data from file
@@ -18,23 +19,19 @@ into arrays, display sorted trading symbols 3 to a line, prompt user for trading
 symbol to search, and display result of trading symbol search with 2 digits
 precision for floating point numbers*/
 void sortSelect(string * symbol, int size);
+int linearSearch(string * symbolsUnsorted, int size, string searchSymbol);
 
 int main() {
+	//Declare variables
 	fstream inpFile;
 	string fileName;
 	string searchSymbol;
-
-	//Initialize variables to 0
+	int foundIndex;
 	int size = 0;
-	//string *symbols = nullptr;
-	//string *names = nullptr;
-	//int *numShares = nullptr;
-	//float *prices = nullptr;
 
 	//Prompt for the file name and open the file
 	cout << "Enter the filename: ";
 	getline(cin, fileName);
-	//fileName = "C:\\Users\\rjbec\\Desktop\\P3_stkPort.txt";
 	inpFile.open(fileName, ios::in);
 
 	//Check if the file exists
@@ -46,16 +43,21 @@ int main() {
 		unique_ptr<string[]> names(new string[size]);
 		unique_ptr<int[]> numShares(new int[size]);
 		unique_ptr<float[]> prices(new float[size]);
-
+		
 		//Read data into parallel arrays
 		int i = 0;
 		while (!inpFile.eof()) {
 			inpFile >> symbols[i];
-			getline(inpFile,names[i], '#');
+			getline(inpFile, names[i], '#');
 			inpFile >> numShares[i] >> prices[i];
-			cout << symbols[i] << names[i] << numShares[i] << prices[i] << endl;
+			//cout << symbols[i] << names[i] << numShares[i] << prices[i] << endl;
 			i++;
 		}
+		//Close the file
+		inpFile.close();
+
+		unique_ptr<string[]> symbolsUnsorted = move(symbols[]);
+		//Sort symbols
 		sortSelect(symbols.get(), size);
 
 		//Output sorted stocks
@@ -63,24 +65,55 @@ int main() {
 		cout << "Available stocks: " << endl;
 		cout << endl;
 		for (int i = 0; i < size; i++) {
+			//Output 3 to a line
 			if (counter % 3 == 0)
 				cout << endl;
-			cout << symbols[i]<<' ';
+			cout << symbols[i] << ' ';
 			counter++;
 		}
+		cout << endl;
+		cout << endl;
+
+		//Prompt for a symbol to search for
 		cout << "Enter the symbol: ";
-		getline(cin,searchSymbol)
+		getline(cin, searchSymbol);
+		cout << endl;
+		cout << endl;
+
+		//Search the array of pointers for the trading symbol
+		foundIndex = linearSearch(symbolsUnsorted.get(), size, searchSymbol);
+
+		//Calculate the total value of all the shares
+		float value = 0;
+		value = prices[foundIndex] * numShares[foundIndex];
+
+		//Output all the data for that trading symbol
+		cout << setprecision(2);
+		cout << fixed;
+		int width = 30;
+		cout << setw(width) << left << "Company Name: " << right << setw(width) << names[foundIndex] << endl;;
+		cout << setw(width) << left << "Number of Shares: " << right << setw(width) << numShares[foundIndex] << endl;;
+		cout << setw(width) << left << "Current Price (per share): " << right << setw(width) << prices[foundIndex] << endl;;
+		cout << setw(width) << left << "Current Value: " << right << setw(width) << value << endl;
+
 
 	}
+	//Exit if the file does not exist
 	else {
 		cout << "Error opening file" << endl;
 		exit(-1);
 	}
-	cout << "File closed" << endl;
-	inpFile.close();
+
 	return 0;
 }
+int linearSearch(string * symbolsUnsorted, int size, string searchSymbol) {
+	for (int i = 0; i < size; i++) {
+		if (symbolsUnsorted[i] == searchSymbol)
+			return i;
+	}
+	return -1;
 
+}
 void sortSelect(string * symbols, int size) {
 	int startScan, minIndex;
 	string minElem;
@@ -88,15 +121,14 @@ void sortSelect(string * symbols, int size) {
 		minIndex = startScan;
 		minElem = symbols[startScan];
 		for (int index = startScan + 1; index < size; index++) {
-
-		if ((symbols[index]) < minElem)
-		{
-			minElem = symbols[index];
-			minIndex = index;
+			if ((symbols[index]) < minElem)
+			{
+				minElem = symbols[index];
+				minIndex = index;
+			}
 		}
+		symbols[minIndex] = symbols[startScan];
+		symbols[startScan] = minElem;
 	}
-	symbols[minIndex] = symbols[startScan];
-	symbols[startScan] = minElem;
-}
-	
+
 }
