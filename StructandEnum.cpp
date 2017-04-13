8ee71491427a047b3cba089dbf03e1d5601b92f6
Robert Beckwith <rjbeckwith55@gmail.com>
2017-04-12 23:24:00 -0500

    working on enum currently does not compile

--- StructandEnum.cpp
+++ StructandEnum.cpp
@@ -16,9 +16,12 @@ struct Car {
 	string carModel;
 	int yearModel;
 	double cost;
+	enum Purpose { BUSINESS = 1, PERSONAL };
 };
 
 void displayCar(Car car[]) {
+	cout << fixed;
+	cout << setprecision(2);
 	cout << left << setw(10) << "Make" << setw(10) << "Model" << setw(10) << "Year" << setw(10) << "Cost" << setw(10) << "Purpose" << endl;
 	for (int i = 0; i < 4; i++) {
 		cout << setw(10) << car[i].carMake;
@@ -33,9 +36,9 @@ void displayCar(Car car[]) {
 
 int main() {
 	Car car[10];
-	car[0] = { "Ford","Taurus",1997,21000.00 };
-	car[1] = { "Honda","Accord",1992,11000.00 };
-	car[2] = { "Lamborghini","Aventador",2011,390000.00 };
+	car[0] = { "Ford","Taurus",1997,21000.00, 1 };
+	car[1] = { "Honda","Accord",1992,11000.00, 1 };
+	car[2] = { "Lamborghini","Aventador",2011,390000.00, 1 };
 	cout << "Enter the car make: ";
 	getline(cin, car[3].carMake);
 	cout << "Enter the car model: ";
