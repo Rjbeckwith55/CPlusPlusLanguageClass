35a05d5f003347e22e70f130e195565b8ac37eeb
Robert Beckwith <rjbeckwith55@gmail.com>
2017-04-12 20:58:23 -0500

    commit

--- StructandEnum.cpp
+++ StructandEnum.cpp
@@ -8,6 +8,7 @@ Descr:
 
 #include <iostream>
 #include <string>
+#include <iomanip>
 using namespace std;
 
 struct Car {
@@ -18,12 +19,12 @@ struct Car {
 };
 
 void displayCar(Car car[]) {
-	cout << "Make" << "Model" << "Year" << "Cost" << "Purpose" << endl;
+	cout << left << setw(10) << "Make" << setw(10) << "Model" << setw(10) << "Year" << setw(10) << "Cost" << setw(10) << "Purpose" << endl;
 	for (int i = 0; i < 4; i++) {
-		cout << car[i].carMake<< " ";
-		cout << car[i].carModel << " ";
-		cout << car[i].yearModel << " ";
-		cout << car[i].cost << " ";
+		cout << setw(10) << car[i].carMake;
+		cout << setw(10) << car[i].carModel;
+		cout << setw(10) << car[i].yearModel;
+		cout << setw(10) << car[i].cost;
 		cout << endl;
 	}
 }
@@ -34,14 +35,14 @@ int main() {
 	Car car[10];
 	car[0] = { "Ford","Taurus",1997,21000.00 };
 	car[1] = { "Honda","Accord",1992,11000.00 };
-	car[2] = { "Lamborghini","Aventador",2011,3900000.00 };
+	car[2] = { "Lamborghini","Aventador",2011,390000.00 };
 	cout << "Enter the car make: ";
 	getline(cin, car[3].carMake);
 	cout << "Enter the car model: ";
 	getline(cin, car[3].carModel);
 	cout << "Enter the car year: ";
 	cin >> car[3].yearModel;
-	cout << "Enter the car cost";
+	cout << "Enter the car cost: ";
 	cin >> car[3].cost;
 	displayCar(car);
 
